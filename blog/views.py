be311dfe8c1ffeb4e97b0dab333d68fc016353dc
İbrahim EDİZ <vektorelpython27@gmail.com>
2021-05-29 14:48:54 +0300

    29052021

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import BlogModel
 from .forms import BlogForm
 
@@ -12,6 +12,14 @@ def gonderidetay(request,pk):
     gonderi = BlogModel.objects.get(pk=pk)
     return render(request,"blog/detay.html",{"gonderi":gonderi})
 
+from django.utils import timezone
 def yeniGonderi(request):
-    form = BlogForm()
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        gonderi = form.save(commit=False)
+        gonderi.yayimzaman = timezone.now()
+        gonderi.save()
+        return redirect('gonderidetay',pk=gonderi.pk)
+    else:
+        form = BlogForm()
     return render(request,"blog/yenigonderi.html",{"form":form})
